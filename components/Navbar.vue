47bdd264af9d0b3ac5fc0bd30e420418560351ec
shubhi1928 <shubhijoshi28@gmail.com>
2022-02-16 02:31:11 +0530

    initial commit

---
+++ components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="px-4 md:px-8 py-4 border border-b-[#E7E8F1] shadow flex flex-col md:flex-row md:items-center md:justify-between">
+    <div class="flex items-center">
+        <InstadappIcon  class="w-8 h-8 text-white"/>
+      <span class="ml-2 font-extrabold text-lg">EXPLORER</span>
+    </div>
+  </div>
+</template>
+<script>
+import { defineComponent } from '@vue/composition-api'
+import InstadappIcon from "~/assets/img/instadapp-logo-icon.svg?inline";
+
+export default defineComponent({
+     components :{
+          InstadappIcon,
+        },
+    setup() {
+        
+    },
+})
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,79 @@
+<template>
+
+     <div>
+       <div class="mt-10">
+            <h2 class="text-xl font-semibold flex flex-start">Overview</h2>
+              <div class="px-1 mt-6 grid w-full grid-cols-1 gap-4 md:grid-cols-3 xl:gap-[18px] ">
+                <div class="shadow rounded-lg py-4 px-4 flex">
+                  <div class="flex-1">
+                    <p class="text-xs text-gray-400 font-medium flex flex start">
+                     Total Assets Supplied
+                    </p>
+                    <div class="flex mb-1 md:flex-wrap">
+                      <h3 class="text-xl text-black-400 font-medium">$1,201,052,129.03</h3>
+                      <div class="ml-1  mt-2.5  text-xs text-green-400 font-medium">+3.56%</div>
+                    </div>
+                    
+                  </div>
+                    <div class="flex items-center ml-1">
+                      <OutgoingIcon  viewBox="0 0 50 50" class="w-10 h-10 fill-current"/>
+                    </div>
+                  </div>
+
+
+                <div class="shadow rounded-lg py-4 px-4 flex">
+                  <div class="flex-1">
+                    <p class="text-xs text-gray-400 font-medium flex flex start">
+                     Total Assets Swaped
+                    </p>
+                    <div class="flex mb-1  mr-1 md:flex-wrap">
+                      <h3 class="text-xl text-black-400 font-medium">$205,654,000.53</h3>
+                      <div class="ml-1 mt-2.5 text-xs text-red-600 font-medium">-13.56%</div>
+                    </div>
+                    
+                  </div>
+                    <div class="flex items-center ml-1">
+                      <IncomingIcon viewBox="0 0 50 50" class="w-10 h-10 fill-current"/>
+                    </div>
+                  </div>
+
+                <div class="shadow rounded-lg py-4 px-4 flex">
+                  <div class="flex-1">
+                    <p class="text-xs text-gray-400 font-medium flex flex start">
+                     Total Assets Borrowed
+                    </p>
+                    <div class="flex mb-1 mr-1 md:flex-wrap">
+                      <h3 class="text-xl text-black-400 font-medium">$205,654,000.53</h3>
+                      <div class="ml-1 mt-2.5  text-xs text-red-600 font-medium">-13.56%</div>
+                    </div>
+                    
+                  </div>
+                    <div class="flex items-center ml-1">
+                      <IncomingIcon viewBox="0 0 50 50" class="w-10 h-10 fill-current"/>
+                    </div>
+                  </div>
+
+
+
+              </div>
+
+            <Transactions/>
+        </div>
+     </div>
+  
+</template>
+
+<script>
+import OutgoingIcon from "~/assets/img/outgoing.svg?inline";
+import IncomingIcon from "~/assets/img/incoming.svg?inline";
+import Transactions from "~/components/Transactions.vue";
+
+export default {
+  components :{
+          OutgoingIcon,
+          IncomingIcon,
+          Transactions
+        },
+  name: 'IndexPage',
+}
+</script>
