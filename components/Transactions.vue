cefe5181aab1ace6c520963aa9957e848aaaedb2
shubhi1928 <shubhijoshi28@gmail.com>
2022-02-24 01:16:24 +0530

    balance explore

--- components/Transactions.vue
+++ components/Transactions.vue
@@ -1,42 +1,36 @@
 <template>
   <div class="pt-5 mb-10">
-    <h1 class="flex flex-start text-gray-400 text-lg mb-2">Recent Transactions</h1>
+   <div class="flex place-content-between">
+     <h1 class="flex flex-start text-gray-400 text-lg mb-2">Assets in Wallet</h1>
+  
+          <input type="text" placeholder="Enter wallet Address" class="border-2 mb-2" v-model="walletAddress" @change="allbalance">
+         
+        </div> 
 
-    <div class=" rounded-md shadow hidden md:block">
+    <div class=" rounded-md shadow  ">
       <table class="w-full font-semibold">
         <thead class=" border-b-2 border-gray-200">
         <tr class="text-gray-400">
-          <th class=" p-3 pl-5 w-32  text-sm font-semibold tracking-wide text-left">DSA ID</th>
-          <th class="p-3 w-40 text-sm font-semibold tracking-wide text-left">Action</th>
-          <th class=" p-3 text-sm font-semibold tracking-wide text-left">Token</th>
-          <th class=" p-3  w-32 text-sm font-semibold tracking-wide text-left">Total Value</th>
-          <th class=" p-3 w-32 text-sm font-semibold tracking-wide text-left">Token Amount</th>
-          <th class=" p-3 w-32 text-sm font-semibold tracking-wide text-left">Token Amount</th>
-          <th class=" p-3 text-sm font-semibold tracking-wide text-left">Transaction Link</th>
+          <th class=" p-3 pl-5 w-32  text-sm font-semibold tracking-wide text-left">Assets</th>
+          <th class=" p-3 text-sm font-semibold tracking-wide text-left">Symbol</th>
+          <th class=" p-3  w-32 text-sm font-semibold tracking-wide text-left">Quantity</th>
+          <th class=" p-3 w-32 text-sm font-semibold tracking-wide text-left">Price</th>
+          <th class=" p-3 w-32 text-sm font-semibold tracking-wide text-left">Value</th>
+          <th class=" p-3 text-sm font-semibold tracking-wide text-left">Contract address</th>
         </tr>
         </thead>
 
         <tbody class="divide-y divide-gray-100">  
 
-        <tr v-for="tran in trans" :key="tran.dsa_id" class="bg-white">
-          <td class="p-2.5 pl-5 text-sm text-gray-400 font-bold whitespace-nowrap text-left">#{{tran.dsa_id}}</td>
-
-          <td class="p-2.5 text-xs whitespace-nowrap text-left">
-             <span v-if="tran.action ==='SWAP'"
-            class="flex w-24 h-6 justify-center items-center  font-medium uppercase tracking-wider text-white bg-blue-600 rounded-md bg-opacity-70">{{tran.action}}</span>
-            <span v-if="tran.action ==='SUPPLIED'"
-            class="flex w-24 h-6 justify-center items-center text-xs font-medium uppercase tracking-wider text-white bg-green-400 rounded-md ">{{tran.action}}</span>
-            <span v-if="tran.action ==='BORROW'"
-            class="flex w-24  h-6 justify-center items-center text-xs font-medium uppercase tracking-wider text-white bg-yellow-500 rounded-md bg-opacity-80">{{tran.action}}</span>
-          </td>
-
-          <td class="p-2.5 text-sm text-gray-700 whitespace-nowrap text-left">{{tran.token}}</td>
-          <td class="p-2.5 text-sm text-gray-700 whitespace-nowrap text-left">${{tran.total_value}}</td>
-          <td class="p-2.5 text-sm text-gray-700 whitespace-nowrap text-left">{{tran.token_amount_to}}</td>
-          <td class="p-2.5 text-sm text-gray-700 whitespace-nowrap text-left">{{tran.token_amount_from}}</td>
+        <tr v-for="tran in trans" :key="tran.symbol" class="bg-white">
+          <td class="p-2.5 pl-5 text-sm text-gray-400 font-bold whitespace-nowrap text-left">{{tran.assets}}</td>
+          <td class="p-2.5 text-sm text-gray-700 whitespace-nowrap text-left">{{tran.symbol}}</td>
+          <td class="p-2.5 text-sm text-gray-700 whitespace-nowrap text-left">{{tran.quantity}}</td>
+          <td class="p-2.5 text-sm text-gray-700 whitespace-nowrap text-left">${{tran.price}}</td>
+          <td class="p-2.5 text-sm text-gray-700 whitespace-nowrap text-left">${{tran.value}}</td>
           <td class="p-2.5 text-sm text-gray-700 whitespace-nowrap text-left">
-            <a :href="tran.link" class="font-bold text-blue-500 hover:underline flex">{{tran.link.slice(8,30)}}...<ExternalLinkIcon class="w-4 h-4"/></a>
-            </td>
+            <a :href="tran.link" class="font-bold text-blue-500 hover:underline flex">{{tran.contractaddress.slice(0,30)}}...<ExternalLinkIcon class="w-4 h-4"/></a>
+          </td>
            
         </tr>
         <tr>
@@ -46,42 +40,15 @@
       </table>
     </div>
 
-    <div class="grid grid-cols-1 sm:grid-cols-2 gap-4 md:hidden">
+    
+    <!-- <form @submit.prevent="allbalance">
+    
+    <input v-model="walletAddress"  @change ="allbalance" placeholder="address" >
 
-      <div v-for="tran in trans" :key="tran.dsa_id" class="bg-white space-y-3 p-4 rounded-lg shadow font-semibold">
-        <div class="flex items-center m-auto space-x-8 text-sm  w-64">
-          <div class="text-sm text-gray-400 font-bold whitespace-nowrap">#{{tran.dsa_id}}</div>
-          <div class="text-sm text-gray-700 whitespace-nowrap">{{tran.token}}</div>
-          <div>
-           <div>${{tran.total_value}}</div>
-          </div>
-        </div>
-         <div class="flex m-auto space-x-4 text-sm h-6 p-auto w-64">
-           <div class="w-10" v-if="tran.token_amount_to===''"> </div>
-          <div class="text-sm text-gray-700 whitespace-nowrap">{{tran.token_amount_to}}</div>
-
-          <div class="text-sm text-gray-700 whitespace-nowrap">             
-            <span v-if="tran.action ==='SWAP'"
-            class="flex w-24 h-6 justify-center items-center  text-xs font-medium uppercase tracking-wider text-white bg-blue-600 rounded-md bg-opacity-70">{{tran.action}}</span>
-            <span v-if="tran.action ==='SUPPLIED'"
-            class="flex w-24 h-6 justify-center items-center text-xs font-medium uppercase tracking-wider text-white bg-green-400 rounded-md ">{{tran.action}}</span>
-            <span v-if="tran.action ==='BORROW'"
-            class="flex w-24  h-6 justify-center items-center text-xs font-medium uppercase tracking-wider text-white bg-yellow-500 rounded-md bg-opacity-80">{{tran.action}}</span>
-          </div>
-
-          <div>
-           <div class="text-sm text-gray-700 whitespace-nowrap ">{{tran.token_amount_from}}</div>
-          </div>
-        </div>
-
-        <div class="text-sm font-medium text-black m-auto  w-68" >
-         <a :href="tran.link" class="font-bold text-blue-500 hover:underline m-auto  w-64 flex">{{tran.link.slice(8,40)}}...<ExternalLinkIcon class="w-4 h-4"/></a>
-        </div>
-       
-      </div>
-
-       <div class=" sm:col-span-2  flex w-full justify-center h-10 items-center"><LeftIcon class="w-3 h-3 text-blue-200 mr-4"/>Page 1 of 7<RightIcon class="w-3 h-3 text-blue-600 ml-4"/></div>
-    </div>
+    </form>
+    <p >{{amount[0].balance}}</p>
+    <p>{{amount[1].balance}}</p>
+     -->
 
 
   </div>
@@ -94,6 +61,7 @@ import { defineComponent } from '@vue/composition-api'
 import ExternalLinkIcon from "~/assets/img/external-link.svg?inline";
 import LeftIcon from "~/assets/img/arrow-left.svg?inline";
 import RightIcon from "~/assets/img/arrow-right.svg?inline";
+// import Web3 from 'web3';
 
 
 export default defineComponent({
@@ -104,103 +72,157 @@ export default defineComponent({
   },
     setup() {
 
-      const trans = [
+      var walletAddress=""
+
+      var trans = [
         {
-          dsa_id:"71242",
-          action:"SWAP",
-          token:"ETH to AVAX",
-          total_value:"2,45m",
-          token_amount_to:"5.4k ETH",
-          token_amount_from: "64.4k AVAX",
-          link: "https://etherscan.io/tx/0xdb80c6a86be4f1fa18dfd235df54b78d3eea8909f024614df46f1aae746f599f"
+          assets:"Ethereum",
+          symbol:"ETH",
+          quantity:"0",
+          price:"100000",
+          value:"0",
+          contractaddress:"-",
+
         },
-        {
-          dsa_id:"71243",
-          action:"SUPPLIED",
-          token:"LEND",
-          total_value:"2,45m",
-          token_amount_to:"5.4k ETH",
-          token_amount_from:"",
-          link: "https://etherscan.io/tx/0xdb80c6a86be4f1fa18dfd235df54b78d3eea8909f024614df46f1aae746f599f"
-        },
-        {
-          dsa_id:"71244",
-          action:"SWAP",
-          token:"ETH to AVAX",
-          total_value:"2,45m",
-          token_amount_to:"5.4k ETH",
-          token_amount_from: "64.4k AVAX",
-          link: "https://etherscan.io/tx/0xdb80c6a86be4f1fa18dfd235df54b78d3eea8909f024614df46f1aae746f599f"
-         },
-          {
-          dsa_id:"71245",
-          action:"BORROW",
-          token:"DAI",
-          total_value:"2,45m",
-          token_amount_to:"",
-          token_amount_from: "64.4k DAI",
-          link: "https://etherscan.io/tx/0xdb80c6a86be4f1fa18dfd235df54b78d3eea8909f024614df46f1aae746f599f"
+         {
+          assets:"INSTADAPP",
+          symbol:"INST",
+          quantity:"0",
+          price:"1.32",
+          value:"0",
+          contractaddress:"0x6f40d4A6237C257fff2dB00FA0510DeEECd303eb",
+
         },
         {
-          dsa_id:"71242",
-          action:"SWAP",
-          token:"ETH to AVAX",
-          total_value:"2,45m",
-          token_amount_to:"5.4k ETH",
-          token_amount_from: "64.4k AVAX",
-          link: "https://etherscan.io/tx/0xdb80c6a86be4f1fa18dfd235df54b78d3eea8909f024614df46f1aae746f599f"
+          assets:"BNB",
+          symbol:"BNB",
+          quantity:"0",
+          price:"442",
+          value:"0",
+          contractaddress:"0xB8c77482e45F1F44dE1745F52C74426C631bDD52",
+
         },
         {
-          dsa_id:"71243",
-          action:"SUPPLIED",
-          token:"LEND",
-          total_value:"2,45m",
-          token_amount_to:"5.4k LEND",
-          token_amount_from:"",
-          link: "https://etherscan.io/tx/0xdb80c6a86be4f1fa18dfd235df54b78d3eea8909f024614df46f1aae746f599f"
+          assets:"SHIBA INU",
+          symbol:"SHIB",
+          quantity:"0",
+          price:"0.001",
+          value:"0",
+          contractaddress:"0x95aD61b0a150d79219dCF64E1E6Cc01f0B64C4cE",
+
         },
         {
-          dsa_id:"71244",
-          action:"SWAP",
-          token:"ETH to AVAX",
-          total_value:"2,45m",
-          token_amount_to:"5.4k ETH",
-          token_amount_from: "64.4k AVAX",
-          link: "https://etherscan.io/tx/0xdb80c6a86be4f1fa18dfd235df54b78d3eea8909f024614df46f1aae746f599f"
-         },
-          {
-          dsa_id:"71245",
-          action:"BORROW",
-          token:"DAI",
-          total_value:"2,45m",
-          token_amount_to:"",
-          token_amount_from: "64.4k DAI",
-          link: "https://etherscan.io/tx/0xdb80c6a86be4f1fa18dfd235df54b78d3eea8909f024614df46f1aae746f599f"
+          assets:"Matic Token",
+          symbol:"MATIC",
+          quantity:"0",
+          price:"1.55",
+          value:"0",
+          contractaddress:"0x7D1AfA7B718fb893dB30A3aBc0Cfc608AaCfeBB0",
+
         },
-          {
-          dsa_id:"71245",
-          action:"BORROW",
-          token:"DAI",
-          total_value:"2,45m",
-          token_amount_to:"",
-          token_amount_from: "64.4k DAI",
-          link: "https://etherscan.io/tx/0xdb80c6a86be4f1fa18dfd235df54b78d3eea8909f024614df46f1aae746f599f"
-        },
-          {
-          dsa_id:"71246",
-          action:"BORROW",
-          token:"DAI",
-          total_value:"2,45m",
-          token_amount_to:"",
-          token_amount_from: "64.4k DAI",
-          link: "https://etherscan.io/tx/0xdb80c6a86be4f1fa18dfd235df54b78d3eea8909f024614df46f1aae746f599f"
+         {
+          assets:"Wrapped Ether",
+          symbol:"WETH",
+          quantity:"0",
+          price:"2655",
+          value:"0",
+          contractaddress:"0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+
         },
 
-      ] 
 
 
-      return{
-        trans
+
+      ]
+
+
+
+      async function getBalanceOfERC20(walletAddress,tokenAddress){
+
+         const Web3 = require('web3');
+         const Web3Client = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io/v3/5a30774b8e5143fabbbb8e724b671741"));
+
+         const minABI = [  
+                // balanceOf
+                {    
+                  constant: true,
+
+                  inputs: [{ name: "_owner", type: "address" }],
+
+                  name: "balanceOf",
+
+                  outputs: [{ name: "balance", type: "uint256" }],
+
+                  type: "function",
+                },
+
+
+
+              ];
+
+                const contract = new Web3Client.eth.Contract(minABI, tokenAddress);
+
+                const result = await contract.methods.balanceOf(walletAddress).call(); 
+  
+                const format = Web3Client.utils.fromWei(result); 
+
+                return format
+      }
+
+
+        async function getEth(walletAddress){
+          const Web3 = require("web3")
+
+           const web3 = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io/v3/5a30774b8e5143fabbbb8e724b671741"))
+
+           const result = await  web3.eth.getBalance(walletAddress);
+            const format = web3.utils.fromWei( web3.utils.toBN(result),"ether");
+            return format
+        }
+
+
+           async function allbalance(){
+
+             for( var i = 1; i<trans.length;i++){
+
+               var data = await getBalanceOfERC20(this.walletAddress, this.trans[i].contractaddress ).then((balance)=>{
+                 this.trans[i].quantity = balance
+               })
+
+               this.trans[i].value = this.trans[i].quantity*this.trans[i].price;
+
+
+             }
+
+             var data1 = await getEth(this.walletAddress).then((balance)=>{
+                 this.trans[0].quantity = balance
+               })
+
+               this.trans[0].value = this.trans[0].quantity*this.trans[0].price;
+
+
+
+
+
+
+             this.walletAddress=""
+
+
+
+
+           }
+
+
+
+
+      
+
+
+
+
+     
+      return{trans,walletAddress,getBalanceOfERC20,allbalance,getEth
+        
       };
     },
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,68 +1,107 @@
 <template>
   <div
-    class="relative  font-sans antialiased text-primary-black">
+    class="relative  font-sans antialiased text-primary-black ">
     <div class="min-h-screen flex flex-col ">
         <Navbar class="sticky top-0 bg-white" />
-     <div class="flex-1  ">
+     <div class="flex-1 ">
         <div class="px-8 md:px-4 max-w-6xl mx-auto text-center">
-     <div>
+     <div >
        <div class="mt-10">
             <h2 class="text-xl font-semibold flex flex-start">Overview</h2>
               <div class="px-1 mt-6 grid w-full grid-cols-1 gap-4 md:grid-cols-3 xl:gap-[18px] ">
                 <div class="shadow rounded-lg py-4 px-4 flex">
                   <div class="flex-1">
                     <p class="text-xs text-gray-400 font-medium flex flex start">
-                     Total Assets Supplied
+                     Net Worth in USD
                     </p>
                     <div class="flex mb-1 md:flex-wrap">
-                      <h3 class="text-xl text-black-400 font-medium">$1,201,052,129.03</h3>
-                      <div class="ml-1  mt-2.5  text-xs text-green-400 font-medium">+3.56%</div>
+                      <h3 class="text-xl text-black-400 font-medium">${{totalUsd}}</h3>
                     </div>
                     
                   </div>
-                    <div class="flex items-center ml-1">
-                      <OutgoingIcon  viewBox="0 0 50 50" class="w-10 h-10 fill-current"/>
-                    </div>
                   </div>
 
 
                 <div class="shadow rounded-lg py-4 px-4 flex">
                   <div class="flex-1">
                     <p class="text-xs text-gray-400 font-medium flex flex start">
-                     Total Assets Swaped
+                     Net Worth in ETH
                     </p>
                     <div class="flex mb-1  mr-1 md:flex-wrap">
-                      <h3 class="text-xl text-black-400 font-medium">$205,654,000.53</h3>
-                      <div class="ml-1 mt-2.5 text-xs text-red-600 font-medium">-13.56%</div>
+                      <h3 class="text-xl text-black-400 font-medium">ETH {{totalETH}}</h3>
                     </div>
                     
                   </div>
-                    <div class="flex items-center ml-1">
-                      <IncomingIcon viewBox="0 0 50 50" class="w-10 h-10 fill-current"/>
-                    </div>
+
                   </div>
 
                 <div class="shadow rounded-lg py-4 px-4 flex">
                   <div class="flex-1">
                     <p class="text-xs text-gray-400 font-medium flex flex start">
-                     Total Assets Borrowed
+                     Total Balance Change (24H)
                     </p>
-                    <div class="flex mb-1 mr-1 md:flex-wrap">
-                      <h3 class="text-xl text-black-400 font-medium">$205,654,000.53</h3>
-                      <div class="ml-1 mt-2.5  text-xs text-red-600 font-medium">-13.56%</div>
-                    </div>
+   
                     
                   </div>
-                    <div class="flex items-center ml-1">
-                      <IncomingIcon viewBox="0 0 50 50" class="w-10 h-10 fill-current"/>
-                    </div>
                   </div>
 
 
 
               </div>
 
-            <Transactions/>
+            <div class="pt-5 mb-10">
+   <div class="flex place-content-between">
+     <h1 class="flex flex-start text-gray-400 text-lg mb-2">Assets in Wallet</h1>
+  
+          <input type="text" placeholder="Enter wallet Address" class="border-2 mb-2" v-model="walletAddress" @change="allbalance">
+         
+        </div> 
+
+    <div class=" rounded-md shadow  ">
+      <table class="w-full font-semibold">
+        <thead class=" border-b-2 border-gray-200">
+        <tr class="text-gray-400">
+          <th class=" p-3 pl-5 w-40  text-sm font-semibold tracking-wide text-left">Assets</th>
+          <th class=" p-3 w-40 text-sm font-semibold tracking-wide text-left">Symbol</th>
+          <th class=" p-3  w-40 text-sm font-semibold tracking-wide text-left">Quantity</th>
+          <th class=" p-3 w-40 text-sm font-semibold tracking-wide text-left">Price</th>
+          <th class=" p-3 w-40 text-sm font-semibold tracking-wide text-left">Value</th>
+          <th class=" p-3 text-sm font-semibold tracking-wide text-left">Contract address</th>
+        </tr>
+        </thead>
+
+        <tbody class="divide-y divide-gray-100">  
+
+        <tr v-for="tran in trans" :key="tran.symbol" class="bg-white">
+          <td class="p-2.5 pl-5 text-sm text-gray-400 font-bold whitespace-nowrap text-left">{{tran.assets}}</td>
+          <td class="p-2.5 text-sm text-gray-700 whitespace-nowrap text-left">{{tran.symbol}}</td>
+          <td class="p-2.5 text-sm text-gray-700 whitespace-nowrap text-left">{{tran.quantity}}</td>
+          <td class="p-2.5 text-sm text-gray-700 whitespace-nowrap text-left">${{tran.price}}</td>
+          <td class="p-2.5 text-sm text-gray-700 whitespace-nowrap text-left">${{tran.value}}</td>
+          <td class="p-2.5 text-sm text-gray-700 whitespace-nowrap text-left">
+            <a :href="getaddress(tran.contractaddress)" class="font-bold text-blue-500 hover:underline flex">{{tran.contractaddress.slice(0,30)}}...<ExternalLinkIcon class="w-4 h-4"/></a>
+          </td>
+           
+        </tr>
+        <tr>
+            <td colspan="7"> <div class=" flex w-full justify-center h-10 items-center"><LeftIcon class="w-3 h-3 text-blue-200 mr-4"/>Page 1 of 7<RightIcon class="w-3 h-3 text-blue-600 ml-4"/></div></td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
+
+    
+    <!-- <form @submit.prevent="allbalance">
+    
+    <input v-model="walletAddress"  @change ="allbalance" placeholder="address" >
+
+    </form>
+    <p >{{amount[0].balance}}</p>
+    <p>{{amount[1].balance}}</p>
+     -->
+
+
+  </div>
         </div>
      </div>
         </div>
@@ -74,21 +113,197 @@
 </template>
 
 <script>
+
+import { defineComponent } from '@vue/composition-api'
+import ExternalLinkIcon from "~/assets/img/external-link.svg?inline";
+import LeftIcon from "~/assets/img/arrow-left.svg?inline";
+import RightIcon from "~/assets/img/arrow-right.svg?inline";
 import OutgoingIcon from "~/assets/img/outgoing.svg?inline";
 import IncomingIcon from "~/assets/img/incoming.svg?inline";
-import Transactions from "~/components/Transactions.vue";
+// import Web3 from 'web3';
 
-export default {
-   layout: 'custom',
-  components :{
-          OutgoingIcon,
-          IncomingIcon,
-          Transactions
+
+export default defineComponent({
+  components:{
+    ExternalLinkIcon,
+    LeftIcon,
+    RightIcon,
+    OutgoingIcon,
+    IncomingIcon,
+  },
+    setup() {
+
+      var walletAddress=""
+      var totalUsd = 0
+      var totalETH = 0
+
+      var trans = [
+        {
+          assets:"Ethereum",
+          symbol:"ETH",
+          quantity:"0",
+          price:"100000",
+          value:"0",
+          contractaddress:"-",
+
         },
-  name: 'IndexPage',
-}
+         {
+          assets:"INSTADAPP",
+          symbol:"INST",
+          quantity:"0",
+          price:"1.32",
+          value:"0",
+          contractaddress:"0x6f40d4A6237C257fff2dB00FA0510DeEECd303eb",
+
+        },
+        {
+          assets:"BNB",
+          symbol:"BNB",
+          quantity:"0",
+          price:"442",
+          value:"0",
+          contractaddress:"0xB8c77482e45F1F44dE1745F52C74426C631bDD52",
+
+        },
+        {
+          assets:"SHIBA INU",
+          symbol:"SHIB",
+          quantity:"0",
+          price:"0.001",
+          value:"0",
+          contractaddress:"0x95aD61b0a150d79219dCF64E1E6Cc01f0B64C4cE",
+
+        },
+        {
+          assets:"Matic Token",
+          symbol:"MATIC",
+          quantity:"0",
+          price:"1.55",
+          value:"0",
+          contractaddress:"0x7D1AfA7B718fb893dB30A3aBc0Cfc608AaCfeBB0",
+
+        },
+         {
+          assets:"Wrapped Ether",
+          symbol:"WETH",
+          quantity:"0",
+          price:"2655",
+          value:"0",
+          contractaddress:"0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+
+        },
+
+
+
+
+      ]
+
+      function getaddress(contract){
+        return "https://etherscan.io/token/"+contract
+      }
+
+
+
+      async function getBalanceOfERC20(walletAddress,tokenAddress){
+
+         const Web3 = require('web3');
+         const Web3Client = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io/v3/5a30774b8e5143fabbbb8e724b671741"));
+
+         const minABI = [  
+                // balanceOf
+                {    
+                  constant: true,
+
+                  inputs: [{ name: "_owner", type: "address" }],
+
+                  name: "balanceOf",
+
+                  outputs: [{ name: "balance", type: "uint256" }],
+
+                  type: "function",
+                },
+
+
+
+              ];
+
+                const contract = new Web3Client.eth.Contract(minABI, tokenAddress);
+
+                const result = await contract.methods.balanceOf(walletAddress).call(); 
+  
+                const format = Web3Client.utils.fromWei(result); 
+
+                return format
+      }
+
+
+        async function getEth(walletAddress){
+          const Web3 = require("web3")
+
+           const web3 = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io/v3/5a30774b8e5143fabbbb8e724b671741"))
+
+           const result = await  web3.eth.getBalance(walletAddress);
+            const format = web3.utils.fromWei( web3.utils.toBN(result),"ether");
+            return format
+        }
+
+
+           async function allbalance(){
+
+             this.totalUsd = 0
+
+             for( var i = 1; i<trans.length;i++){
+
+               var data = await getBalanceOfERC20(this.walletAddress, this.trans[i].contractaddress ).then((balance)=>{
+                 this.trans[i].quantity = balance
+               })
+
+               this.trans[i].value = this.trans[i].quantity*this.trans[i].price;
+
+               this.totalUsd += this.trans[i].value
+
+
+             }
+
+             var data1 = await getEth(this.walletAddress).then((balance)=>{
+                 this.trans[0].quantity = balance
+               })
+
+               this.trans[0].value = this.trans[0].quantity*this.trans[0].price;
+
+               this.totalUsd += this.trans[0].value
+
+               this.totalETH = this.totalUsd/ this.trans[0].price
+
+
+
+
+
+
+             this.walletAddress=""
+
+
+
+
+           }
+
+
+
+
+      
+
+
+
+
+     
+      return{trans,walletAddress,getBalanceOfERC20,allbalance,getEth,totalUsd,totalETH,getaddress
+        
+      };
+    },
+})
 </script>
-<style>
+<style scoped>
 
 </style>
+
 
